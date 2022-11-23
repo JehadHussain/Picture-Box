9085491dc6da42137e3746618cc4dd64196be390
jehad <jehad@LAPTOP-B0JROT3V>
2022-11-23 23:25:53 +0300

    Add project files.

---
+++ Picture Box/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Image image = Image.FromFile(ofd.FileName);
+                pictureBox1.Image = image;
+            }
+        }
+    }
+}
+
